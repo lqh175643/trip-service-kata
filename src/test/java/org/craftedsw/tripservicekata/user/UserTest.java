38ca52369bdb976aaf2df6fa61a8dac82e31d059
wj <linwenjun@163.com>
2022-07-13 16:27:38 +0800

    first commit

---
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -0,0 +1,13 @@
+package org.craftedsw.tripservicekata.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    @Test
+    void name() {
+        assertEquals(1, 1);
+    }
+}
