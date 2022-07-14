ecb16da06f56d7aeded00d6ab9b5444a32dd6de6
Qiuhong Lei <1756431742@qq.com>
2022-07-14 17:24:58 +0800

    test: 添加User测试

--- src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -4,7 +4,7 @@ import org.craftedsw.tripservicekata.trip.Trip;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
 
@@ -33,5 +33,17 @@ public class UserTest {
     void should_has_one_trip_when_add_a_trip(){
         user.addTrip(new Trip());
         assertEquals(user.getTrips().size(),1);
+    }
+
+    @Test
+    void should_return_true_when_they_are_friends(){
+        User friend = new User();
+        user.addFriend(friend);
+        assertTrue(User.isFriend(user,friend));
+    }
+
+    @Test
+    void should_return_false_when_they_are_not_friends(){
+        assertFalse(User.isFriend(user,new User()));
     }
 }
